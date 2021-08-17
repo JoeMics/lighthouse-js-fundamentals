6acdb0e7201a03aed5a9dd2f0e170dccf1e84e6e
Joseph Micla <josephmicla@gmail.com>
2021-08-17 18:35:52 +0000

    added solution to Trash To Treasure

---
+++ objects-trash-to-treasure.js
@@ -0,0 +1,7 @@
+const smartGarbage = function (trash, bins) {
+  bins[trash]++;
+  return bins;
+}
+
+// test case
+console.log(smartGarbage('recycling', { waste: 4, recycling: 2, compost: 5 })); //{ waste: 4, recycling: 3, compost: 5 }
